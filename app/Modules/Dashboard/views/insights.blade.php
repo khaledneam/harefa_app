dba3caed47ec4686b8a1fe2c49c66d185bf42c22
khaled neam <khaledneam27@gmail.com>
2024-09-27 20:32:27 +0300

    support and insights 1

--- app/Modules/Dashboard/views/insights.blade.php
+++ app/Modules/Dashboard/views/insights.blade.php
@@ -9,27 +9,27 @@
             @csrf
             <div class="col-md-12 form-group">
                 <label>عدد الاسئله</label>
-                <input type="number" name="question_count" required value="{{ $insights->question_count }}">
+                <input type="number" name="question_count" required value="{{ $insights->question_count??null }}">
             </div>
             <div class="col-md-12 form-group">
                 <label> عدد الاحكام</label>
-                <input type="number" name="count_judgments" required value="{{$insights->count_judgments}}">
+                <input type="number" name="count_judgments" required value="{{$insights->count_judgments ?? null}}">
             </div>
             <div class="col-md-12 form-group">
                 <label> عدد التحدى </label>
-                <input type="number" name="count_challenges" required value="{{$insights->count_challenges}}">
+                <input type="number" name="count_challenges" required value="{{$insights->count_challenges ?? null}}">
             </div>
             <div class="col-md-12 form-group">
                 <label> عدد الباقات </label>
-                <input type="number" name="count_packages" required value="{{ $insights->count_packages }}">
+                <input type="number" name="count_packages" required value="{{ $insights->count_packages ?? null}}">
             </div>
             <div class="col-md-12 form-group">
                 <label> عدد الاعبين </label>
-                <input type="number" name="count_players" required value="{{ $insights->count_players }}">
+                <input type="number" name="count_players" required value="{{ $insights->count_players ?? null}}">
             </div>
             <div class="col-md-12 form-group">
                 <label> عدد الدوريات </label>
-                <input type="number" name="count_leagues" required value="{{ $insights->count_leagues }}">
+                <input type="number" name="count_leagues" required value="{{ $insights->count_leagues ?? null }}">
             </div>
 
             <button type="submit" name="">Save</button>
